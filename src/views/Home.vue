2d3a98ea3edc0bf08c4859cd5be6d37b84b2734d
Jann Dave <jdapohin@gmail.com>
2024-06-10 10:08:36 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex flex-col p-8">
+        <input type="text" class="rounded borded-2 border-gray-200 w-full" 
+            placeholder = "Search for meals">
+    
+        <div class="flex gap-2 mt-2 justify-center">
+            <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+            {{ letter }}
+            </router-link>
+        </div>
+        
+       <!-- <pre>{{ ingredients }}</pre> -->
+    </div>   
+</template>
+
+<script setup>
+import { computed, onMounted, ref } from 'vue';
+import store from '../store'
+import axiosClient from '../axiosClient.js'
+
+
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+const ingredients = ref([])
+
+onMounted(async () => {
+    const response = await axiosClient.get('/list.php?i=list')
+    console.log(response.data)
+    ingredients.value = response.data
+})
+
+</script>
